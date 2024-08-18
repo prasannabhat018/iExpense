e74597910b23ed18f74a6296822f4a2f1b6d18f2
Prasanna Bhat <prasannabhat018@gmail.com>
2024-08-18 13:27:30 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Prasanna Bhat on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
